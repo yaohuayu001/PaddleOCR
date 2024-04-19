047faa1e8b86de3f5f2f00926e8b99da56037f61
yaohuayu001 <1652750395@qq.com>
2024-04-20 02:42:57 +0800

    1

--- test1.py
+++ test1.py
@@ -6,7 +6,7 @@ paddle.utils.run_check()
 from paddleocr import PaddleOCR
  
 ocr = PaddleOCR(use_gpu=True,lang='ch',use_angle_cls=True)  # need to run only once to download and load model into memory
-img_path = 'doc/imgs_words/en/word_5.png'
+img_path = 'doc/imgs_words/en/yes.png'
 result = ocr.ocr(img_path, det=False)
 for line in result:
     print(line)
